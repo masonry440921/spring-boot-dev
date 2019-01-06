1e733faad2ba75e3964e185abaf630a28ac4e510
masonry440921 <free-masonry@hotmail.com>
2019-01-06 22:49:36 +0800

    25

---
+++ mason-springboot-part25/src/main/java/com/mason/profilesConfig/MasonSpringbootPart25Application.java
@@ -0,0 +1,14 @@
+package com.mason.profilesConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasonSpringbootPart25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasonSpringbootPart25Application.class, args);
+	}
+
+}
+
